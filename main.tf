52c9831352aac17b2fe18f249bbc5c464dad63bd
MagenX <magenx@users.noreply.github.com>
2025-05-16 10:57:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1231,7 +1231,7 @@ module "ecs_service" {
   }
   load_balancer = {
     service = {
-      target_group_arn = module.alb.target_groups
+      target_group_arn = module.alb.target_groups.arn
       container_name   = local.env.ecs.container_name
       container_port   = local.env.ecs.container_port
     }
