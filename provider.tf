7b8246f67514f045e6306a51271d1096d64f1a35
MagenX <magenx@users.noreply.github.com>
2025-05-22 07:44:18 +0200

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,31 @@
+
+
+//////////////////////////////////////////////////////////[ PROVIDER ]////////////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Define provider
+# # ---------------------------------------------------------------------------------------------------------------------#
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.95.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.7.2"
+    }
+  }
+}
+provider "aws" {
+  default_tags {
+   tags = local.default_tags
+ }
+}
+provider "aws" {
+  alias  = "us-east-1"
+  region = "us-east-1"
+  default_tags {
+   tags = local.default_tags
+ }
+}
