1ca4cf27c5c9e08d1ef4f8a81463db796e291cff
MagenX <magenx@users.noreply.github.com>
2025-05-22 07:48:30 +0200

    Create sns.tf

---
+++ sns.tf
@@ -0,0 +1,20 @@
+
+
+/////////////////////////////////////////////////////[ SNS TOPICS MODULE ]////////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create SNS topics for alerts subscription
+# # ---------------------------------------------------------------------------------------------------------------------#
+module "sns" {
+  source   = "terraform-aws-modules/sns/aws"
+  version  = "6.1.3"
+  for_each = local.env.sns.topic
+  name     = "${local.project}-${each.key}"
+  subscriptions = {
+    for email_address in each.value.email :
+    "email" => {
+      protocol = "email"
+      endpoint = email_address
+    }
+  }
+}
