3ce252147d95abab661f4cc2d3606b8fce14677d
MagenX <magenx@users.noreply.github.com>
2025-05-22 07:47:56 +0200

    Create acm.tf

---
+++ acm.tf
@@ -0,0 +1,27 @@
+
+
+///////////////////////////////////////////////////////[ ACM SSL MODULE ]/////////////////////////////////////////////////
+
+# # ---------------------------------------------------------------------------------------------------------------------#
+# Create ACM certificates for cloudfront and alb
+# # ---------------------------------------------------------------------------------------------------------------------#
+module "acm" {
+  source                    = "terraform-aws-modules/acm/aws"
+  version                   = "5.1.1"
+  domain_name               = local.env.domain
+  validation_method         = "DNS"
+  subject_alternative_names = concat(compact(local.env.san), compact(local.env.aliases))
+  create_route53_records    = false
+  validate_certificate      = false
+}
+module "acm_cloudfront" {
+  source                    = "terraform-aws-modules/acm/aws"
+  version                   = "5.1.1"
+  count                     = local.use_us_east_1 ? 1 : 0
+  providers                 = { aws = aws.us-east-1 }
+  domain_name               = local.env.domain
+  validation_method         = "DNS"
+  subject_alternative_names = concat(compact(local.env.san), compact(local.env.aliases))
+  create_route53_records    = false
+  validate_certificate      = false
+}
