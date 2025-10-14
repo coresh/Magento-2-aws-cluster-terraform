1ba17d69b6b89dcc32b574dc4e85c359dc1a70df
MagenX <magenx@users.noreply.github.com>
2025-10-14 14:26:01 +0200

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -39,7 +39,7 @@ resource "aws_service_discovery_private_dns_namespace" "this" {
 resource "aws_service_discovery_service" "this" {
   name = "service"
   dns_config {
-    namespace_id = aws_service_discovery_private_dns_namespace.service.id
+    namespace_id = aws_service_discovery_private_dns_namespace.this.id
     dns_records {
       type = "A"
       ttl  = 10
