933c2aba9412995ca820723c7bbaee83e6a2eb68
MagenX <magenx@users.noreply.github.com>
2025-10-14 14:35:24 +0200

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -47,7 +47,6 @@ resource "aws_service_discovery_service" "this" {
    routing_policy = "MULTIVALUE"
   }
   health_check_custom_config {
-    failure_threshold = 1
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
