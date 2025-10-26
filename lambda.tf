ed16c96804eb4ac0285a3a2df8e500a5a32b99db
MagenX <magenx@users.noreply.github.com>
2025-10-26 17:54:58 +0100

    Update environment variables and IAM policy in lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -19,25 +19,27 @@ module "imgproxy" {
   timeout        = local.env.lambda.timeout
   architectures  = ["arm64"]
   environment_variables = {
-    PORT                               = local.env.imgproxy.port
-    IMGPROXY_USE_S3                    = local.env.imgproxy.use_s3
-    IMGPROXY_ALLOW_INSECURE            = local.env.imgproxy.allow_insecure
-    IMGPROXY_ALLOWED_SOURCES           = local.env.imgproxy.allowed_sources
-    IMGPROXY_FALLBACK_IMAGE_PATH       = local.env.imgproxy.fallback_image_path
-    IMGPROXY_WATERMARK_PATH            = local.env.imgproxy.watermark_path
-    IMGPROXY_MAX_REDIRECTS             = local.env.imgproxy.max_redirects
-    IMGPROXY_TTL                       = local.env.imgproxy.ttl
-    IMGPROXY_AUTO_WEBP                 = local.env.imgproxy.auto_webp
-    IMGPROXY_AUTO_AVIF                 = local.env.imgproxy.auto_avif
-    IMGPROXY_QUALITY                   = local.env.imgproxy.quality
-    IMGPROXY_FORMAT_QUALITY            = local.env.imgproxy.format_quality
-    IMGPROXY_USE_ETAG                  = local.env.imgproxy.use_etag
-    IMGPROXY_ENABLE_DEBUG_HEADERS      = local.env.imgproxy.enable_debug_headers
-    IMGPROXY_LOG_FORMAT                = local.env.imgproxy.log_format
-    IMGPROXY_LOG_LEVEL                 = local.env.imgproxy.log_level
-    IMGPROXY_CLOUD_WATCH_SERVICE_NAME  = local.env.imgproxy.cloud_watch_service_name
-    IMGPROXY_CLOUD_WATCH_NAMESPACE     = local.env.imgproxy.cloud_watch_namespace
-    IMGPROXY_CLOUD_WATCH_REGION        = data.aws_region.current.region
+    PORT                                = local.env.imgproxy.port
+    IMGPROXY_USE_S3                     = local.env.imgproxy.use_s3
+    IMGPROXY_ALLOW_INSECURE             = local.env.imgproxy.allow_insecure
+    IMGPROXY_ALLOWED_PROCESSING_OPTIONS = local.env.imgproxy.allowed_processing_options
+    IMGPROXY_ALLOW_ORIGIN               = local.env.imgproxy.allow_origin
+    IMGPROXY_ALLOWED_SOURCES            = local.env.imgproxy.allowed_sources
+    IMGPROXY_FALLBACK_IMAGE_PATH        = local.env.imgproxy.fallback_image_path
+    IMGPROXY_WATERMARK_PATH             = local.env.imgproxy.watermark_path
+    IMGPROXY_MAX_REDIRECTS              = local.env.imgproxy.max_redirects
+    IMGPROXY_TTL                        = local.env.imgproxy.ttl
+    IMGPROXY_AUTO_WEBP                  = local.env.imgproxy.auto_webp
+    IMGPROXY_AUTO_AVIF                  = local.env.imgproxy.auto_avif
+    IMGPROXY_QUALITY                    = local.env.imgproxy.quality
+    IMGPROXY_FORMAT_QUALITY             = local.env.imgproxy.format_quality
+    IMGPROXY_USE_ETAG                   = local.env.imgproxy.use_etag
+    IMGPROXY_ENABLE_DEBUG_HEADERS       = local.env.imgproxy.enable_debug_headers
+    IMGPROXY_LOG_FORMAT                 = local.env.imgproxy.log_format
+    IMGPROXY_LOG_LEVEL                  = local.env.imgproxy.log_level
+    IMGPROXY_CLOUD_WATCH_SERVICE_NAME   = local.env.imgproxy.cloud_watch_service_name
+    IMGPROXY_CLOUD_WATCH_NAMESPACE      = local.env.imgproxy.cloud_watch_namespace
+    IMGPROXY_CLOUD_WATCH_REGION         = data.aws_region.current.region
   }
   create_lambda_function_url        = true
   authorization_type                = "AWS_IAM"
@@ -56,6 +58,14 @@ module "imgproxy" {
           "cloudwatch:PutMetricStream"
         ]
         Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion"
+        ]
+        Resource = "${module.s3["media"].s3_bucket_arn}/*"
       }
     ]
   })
