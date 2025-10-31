d1c10ebd33c819402f1b8ae85f5f03c9e07001da
MagenX <magenx@users.noreply.github.com>
2025-10-31 14:21:03 +0100

    Update ssm_role.tf

--- ssm_role.tf
+++ ssm_role.tf
@@ -6,58 +6,61 @@
 # # ---------------------------------------------------------------------------------------------------------------------#
 # Create SSM role to execute automations and documents
 # # ---------------------------------------------------------------------------------------------------------------------#
-data "aws_iam_policy_document" "ssm_assume_role" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ssm.amazonaws.com"]
-    }
-  }
-}
-resource "aws_iam_role" "ssm_service_role" {
-  name               = "${local.project}-SSMServiceRole"
-  description        = "Provides SSM manage automations on your behalf."
-  assume_role_policy = data.aws_iam_policy_document.ssm_assume_role.json
-}
 data "aws_iam_policy_document" "ssm_policy" {
   statement {
     effect    = "Allow"
     actions   = [
-      "ec2:Describe*",
-      "codedeploy:CreateDeployment",
-      "codedeploy:GetDeploymentConfig",
-      "codedeploy:RegisterApplicationRevision",
-      "codedeploy:GetApplicationRevision",
-      "tag:GetResources",
+      "codebuild:StartBuild",
+      "codebuild:BatchGetBuilds",
+    ]
+    resources = [aws_codebuild_project.this.arn]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
       "sns:Publish",
-      "ssm:DescribeInstanceInformation",
+    ]
+    resources = [module.sns["devops"].topic_arn]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
       "ssm:StartAutomationExecution",
       "ssm:GetAutomationExecution",
-      "ssm:SendCommand",
-      "ssm:ListCommands",
-      "ssm:ListCommandInvocations",
+    ]
+    resources = [aws_ssm_document.release.arn]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
       "ssm:GetParameter",
       "ssm:GetParameters",
-      "route53:CreateHealthCheck",
-      "servicediscovery:Get*",
-      "servicediscovery:List*",
-      "servicediscovery:RegisterInstance",
-      "servicediscovery:DeregisterInstance",
+    ]
+    resources = ["arn:aws:ssm:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:parameter/${local.project}/*"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = [
+      "ec2:DescribeInstances",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeSecurityGroups",
     ]
     resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:PrincipalAccount"
-      values   = [data.aws_caller_identity.current.account_id]
-    }
   }
 }
+
+resource "aws_iam_role" "ssm_service_role" {
+  name               = "${local.project}-SSMServiceRole"
+  description        = "Provides SSM manage automations and trigger CodeBuild"
+  assume_role_policy = data.aws_iam_policy_document.ssm_assume_role.json
+}
+
 resource "aws_iam_policy" "ssm_policy" {
   name   = "${local.project}-SSMPolicy"
   policy = data.aws_iam_policy_document.ssm_policy.json
 }
+
 resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
   role       = aws_iam_role.ssm_service_role.name
   policy_arn = aws_iam_policy.ssm_policy.arn
