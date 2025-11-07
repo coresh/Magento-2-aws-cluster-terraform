7a40eb982e2e2095bc4f882304db45c5c1c12505
MagenX <magenx@users.noreply.github.com>
2025-11-07 14:39:44 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -28,6 +28,129 @@ resource "aws_ssm_parameter" "s3" {
   }
 }
 # # ---------------------------------------------------------------------------------------------------------------------#
+# Create S3 bucket policy
+# # ---------------------------------------------------------------------------------------------------------------------#
+data "aws_iam_policy_document" "logs" {
+  statement {
+    sid    = "ALBS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = ["${module.s3["logs"].arn}/ALB_logs/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [module.alb.arn]
+    }
+  }
+  statement {
+    sid    = "CloudFrontS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = ["${module.s3.["logs"].s3_bucket_arn}/cloudfront/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "AWS:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "releases" {
+ statement {
+    sid    = "AllowCodebuildS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket",
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning"
+    ]
+    resources = [
+      "${module.s3["releases"].s3_bucket_arn}",
+      "${module.s3["releases"].s3_bucket_arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers =  aws_iam_role.codebuild.arn
+    }
+  }
+
+data "aws_iam_policy_document" "media" {
+  statement {
+    sid       = "ECSS3Access"
+    effect    = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:ListBucket"
+    ]
+    resources = [
+      "${moduel.s3["media"].arn}",
+      "${moduel.s3["media"].arn}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = [module.ecs_cluster.task_exec_iam_role_arn]
+    }
+  }
+  statement {
+    sid       = "LambdaS3Access"
+    effect    = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = ["${moduel.s3["media"].arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [module.imgproxy.lambda_function_arn]
+    }
+  }
+  statement {
+    sid    = "CloudFrontS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+    resources = ["${module.s3.["media"].s3_bucket_arn}/*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+    condition {
+      test     = "ArnLike"
+      variable = "AWS:SourceArn"
+      values   = [module.cloudfront.cloudfront_distribution_arn]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "backup" {
+  statement {
+    sid    = "SSMS3Access"
+    effect = "Allow"
+    actions = [
+      "s3:PutObject"
+    ]
+    resources = ["${module.s3["backup"].arn}/*"]
+    principals {
+      type        = "AWS"
+      identifiers = [aws_iam_role.ssm_service_role.arn]
+    }
+  }
+
+locals {
+  bucket_policy = {
+    for name in local.env.s3.bucket : name => data.aws_iam_policy_document[name].json
+  }
+}
+
+# # ---------------------------------------------------------------------------------------------------------------------#
 # Create S3 buckets
 # # ---------------------------------------------------------------------------------------------------------------------#
 module "s3" {
@@ -36,8 +159,8 @@ module "s3" {
   for_each = local.env.s3.bucket
   bucket   = "${local.project}-${each.key}"
   acl      = "private"
-  #attach_policy           = true
-  #policy                  = each.value.policy
+  attach_policy            = true
+  policy                   = local.bucket_policy[each.key]
   force_destroy            = true
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
@@ -77,8 +200,8 @@ module "s3" {
 # Configure S3 buckets notifications for example to eventbridge
 # # ---------------------------------------------------------------------------------------------------------------------#
 module "s3_notifications" {
-  source   = "terraform-aws-modules/s3-bucket/aws//modules/notification"
-  for_each = local.env.s3.bucket
-  bucket   = module.s3[each.key].s3_bucket_id
+  source      = "terraform-aws-modules/s3-bucket/aws//modules/notification"
+  for_each    = local.env.s3.bucket
+  bucket      = module.s3[each.key].s3_bucket_id
   eventbridge = each.value.eventbridge
 }
