4ae51f36f8a331dd52139c5b0482c47b55df0c42
MagenX <magenx@users.noreply.github.com>
2025-11-07 19:43:58 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -40,7 +40,7 @@ data "aws_iam_policy_document" "logs" {
     resources = ["${module.s3["logs"].s3_bucket_arn}/ALB_logs/*"]
     principals {
       type        = "AWS"
-      identifiers = [module.alb.arn]
+      identifiers = [logdelivery.elasticloadbalancing.amazonaws.com]
     }
   }
   statement {
